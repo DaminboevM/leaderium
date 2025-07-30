52450ac76c206a1d033a0fcbce5b8c0f76d09688
DaminboevM <m701rizo@gmail.com>
2025-07-30 13:33:26 +0500

    second commit

--- src/core/decorators/roles.decorator.ts
+++ src/core/decorators/roles.decorator.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from "@nestjs/common";
-import { UserRole } from "src/common/types/types";
+import { UserRole } from "src/common/types/type"; 
 
 export const Roles = (...roles: UserRole[]) => SetMetadata('roles', roles)
